778c83123143c94616524e2be0331bf7640cc4d9
Rainer Mesi <rainer.mesi@gmail.com>
2023-07-19 19:47:28 +0000

    updated shell script

--- get_keyfile.sh
+++ get_keyfile.sh
@@ -3,11 +3,11 @@
 # create a tmp folder
 mkdir -p ./tmp
 
-# create a temproary keyfile
-echo $SA_JSON >./tmp/keyfile.json
-
 # write secret to temporary keyfile
 export SA_JSON=$SA_LOOK_ECOMMERCE
+
+# create a temproary keyfile
+echo $SA_JSON >./tmp/keyfile.json
 
 # make the .sh runnable with
 # chmod +x get_keyfile.sh
